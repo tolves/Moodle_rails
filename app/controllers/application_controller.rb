e89900321e8ca3c3adbd31a9de1d4dbd0e298626
tolves <tov.sharing@gmail.com>
2020-11-22 11:24:50 +0000

    back to login with email..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: attrs
-    devise_parameter_sanitizer.permit :account_update, keys: attrs
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
