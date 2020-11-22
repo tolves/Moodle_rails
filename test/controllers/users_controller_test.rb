8354fde4539691aa2efbe2e8597da65b560752db
tolves <tov.sharing@gmail.com>
2020-11-22 11:28:39 +0000

    user test case

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'GET new' do
+    # Mimic the router behavior of setting the Devise scope through the env.
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+
+    # Use the sign_in helper to sign in a fixture `User` record.
+    sign_in users
+
+    get :new
+
+    # assert something
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# set test db
+# add ActiveRecord::Migration.maintain_test_schema! to the top of the test_helper.rb file.
+# rake test:prepare
+# rake db:migrate
+# rake db:migrate RAILS_ENV=test
+# rake db:seed RAILS_ENV=test
+
+
+ActiveRecord::Migration.maintain_test_schema!
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
@@ -10,4 +20,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include Devise::Test::IntegrationHelpers
 end
