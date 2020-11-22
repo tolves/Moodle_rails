1bdf7295e48cfbb134c1779a26ea1c1876644592
tolves <tov.sharing@gmail.com>
2020-11-22 14:17:59 +0000

    login&logout test case successful..so tired

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,19 +1,49 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  test 'GET new' do
-    # Mimic the router behavior of setting the Devise scope through the env.
-    @request.env['devise.mapping'] = Devise.mappings[:user]
-
-    # Use the sign_in helper to sign in a fixture `User` record.
-    sign_in users
-
-    get :new
-
-    # assert something
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @user = users(:foo)
+    get new_user_session_path
+    sign_in @user
   end
 
+  test 'log in & browse' do
+    visit root_path
+    logout @user
+  end
+
+  private
+
+  def logout(user)
+    delete destroy_user_session_path
+    assert_redirected_to root_path
+  end
+
+  def visit(path)
+    get path
+    assert_select 'p', text: 'welcome index'
+    assert_equal 200, status
+    assert_select 'a[href=?]', destroy_user_session_path
+  end
+
+  # test 'Log in & Browse' do
+  #   get "/users"
+  #   assert_redirected_to new_user_session_path
+  #   # get the login page
+  #   get "/users/sign_in"
+  #   assert_equal 200, status
+  #   # post the login and follow through to the home page
+  #   # post '/users/sign_in', params: { email: users(:alice).email, password: 'password' }
+  #   # assert_select "a[href=?]", destroy_user_session_path
+  #   sign_in users(:alice)
+  #   puts response.flashes
+  #   delete destroy_user_session_path
+  #   assert_equal 302, status
+  #   assert_redirected_to root_path
+  #   # puts response.body
+  #
+  #   post user_session_path, params: { email: users(:alice).email, password: 111 }
+  #   # puts response.body
+  # end
 end
