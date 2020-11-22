3df0b4519c626f7de6e6059d7e50f138d28f8846
tolves <tov.sharing@gmail.com>
2020-11-22 22:35:46 +0000

    fix bug: user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#index'
-
-  resources :users
+  # if devise under resources :users
+  # log out will find users#destroy instead of devise#destroy
   devise_for :users
+  resources :users
 
   resources :courses
 
