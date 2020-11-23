0ca2ad2157e9f3daac04af305ffcd2646f0615b6
tolves <tov.sharing@gmail.com>
2020-11-23 18:38:30 +0000

    set admin dashboard by hash

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,50 @@
 class AdminController < ApplicationController
   def index
     @menu = %w[a b c d]
+    @nav_items = %w[site_administration users courses grades plugins apperance server reports development]
+
+    @tabs = {
+      users: {
+        accounts: {
+          'browse_list_users' => admin_users_path,
+          'add_a_new_user' => new_admin_user_path,
+          'user_default_preferences' => '',
+          'User_customise_fields' => '',
+          'group' => ''
+        },
+        permissions: {
+          'manage_roles' => '',
+          'define_roles' => ''
+        }
+      },
+      site_administration: {
+        '': {
+          'notifications' => '',
+          'registration' => '',
+          'moodle_services' => '',
+          'feedback_settings' => '',
+          'advanced_features' => '',
+        },
+        analytics: {
+          'site_information' => '',
+          'analytics_settings' => '',
+          'analytics_models' => '',
+        },
+        language: {
+          'language_settings' => '',
+          'language_customisation' => '',
+          'language_packs' => '',
+        },
+        location: {
+          'location_settings' => '',
+        },
+        badges: {
+          'manage_badges' => '',
+          'add_new_badges' => '',
+          'badges_settings' => '',
+        }
+      }
+    }
   end
 
   def show; end
