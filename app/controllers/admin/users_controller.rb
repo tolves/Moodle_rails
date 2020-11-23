13243ce89a3b09621ab6be4b5cd8bad65eb4d533
tolves <tov.sharing@gmail.com>
2020-11-23 14:05:59 +0000

    i18n

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,37 +17,24 @@ class Admin::UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    flash.notice, path = if account_update_params[:password].blank?
-                           if update_skip_password user, account_update_params
-                             ["Change User: #{user.email} Profile Without Password Successful", admin_user_path(user.id)]
-                           else
-                             [user.errors, edit_admin_user_path(user.id)]
-                           end
-                         else
-                           if user.update(account_update_params)
-                             ["Change User: #{user.email} Profile Successful", admin_user_path(user.id)]
-                           else
-                             [user.errors, edit_admin_user_path(user.id)]
-                           end
-                         end
+    path = if account_update_params[:password].blank?
+             if update_skip_password user, account_update_params
+               flash[:success] = t('.success', email: user.email)
+               admin_user_path(user.id)
+             else
+               flash[:warning] = user.errors.messages
+               edit_admin_user_path(user.id)
+             end
+           else
+             if user.update(account_update_params)
+               flash[:success] = t('.success', email: user.email)
+               admin_user_path(user.id)
+             else
+               flash[:warning] = user.errors.messages
+               edit_admin_user_path(user.id)
+             end
+           end
     redirect_to path
-
-    # self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    # prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-    #
-    # resource_updated = update_resource(resource, account_update_params)
-    # yield resource if block_given?
-    # if resource_updated
-    #   set_flash_message_for_update(resource, prev_unconfirmed_email)
-    #   bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
-    #
-    #   respond_with resource, location: after_update_path_for(resource)
-    # else
-    #   clean_up_passwords resource
-    #   set_minimum_password_length
-    #   respond_with resource
-    # end
-
   end
 
   def destroy; end
