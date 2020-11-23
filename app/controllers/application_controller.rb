5aa888eabc2df8ac19526a4501f199937e924d00
tolves <tov.sharing@gmail.com>
2020-11-23 14:06:53 +0000

    flash alert&notice => alert-warning&success

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Permission
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :flash_format
 
   protected
 
@@ -11,5 +12,16 @@ class ApplicationController < ActionController::Base
 
     account_update_attrs = %i[password password_confirmation current_password givenname surname role]
     devise_parameter_sanitizer.permit(:account_update, keys: account_update_attrs)
+  end
+
+  def flash_format
+    unless alert.blank?
+      flash[:warning] = alert
+      flash.delete(:alert)
+    end
+    unless notice.blank?
+      flash[:primary] = notice
+      flash.delete(:notice)
+    end
   end
 end
