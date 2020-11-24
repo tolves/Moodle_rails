671bb4a28f859e6fc599073b1e0d3ebb2ed2405c
tolves <tov.sharing@gmail.com>
2020-11-24 23:42:52 +0000

    create migration users <=> courses

---
+++ db/migrate/20201124231434_user_has_and_belongs_to_many_course.rb
@@ -0,0 +1,8 @@
+class UserHasAndBelongsToManyCourse < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users_courses, id: false do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :course, index: true
+    end
+  end
+end
