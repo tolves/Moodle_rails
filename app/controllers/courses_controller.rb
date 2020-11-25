a71c182c95c5c49f189857ace1cdfc0e8ee00d23
tolves <tov.sharing@gmail.com>
2020-11-25 22:23:30 +0000

    add courses(simple template)

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,29 @@
 class CoursesController < ApplicationController
-  def index; end
+  def index
+    @courses = Course.all
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @course = Course.new
+  end
 
-  def create; end
+  def create
+    @course = Course.new params_course
+    @course.save
+    redirect_to courses_path
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def params_course
+    params.require(:course).permit([:name])
+  end
 end
