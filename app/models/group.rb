f36b0bc4b9b0f60daee4495660928cd175191542
tolves <tov.sharing@gmail.com>
2020-11-26 23:00:35 +0000

    validates true

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :courses
-  has_many :type_policies, as: :subject
+  has_many :type_policies, as: :subject, validate: true
 end
