ea91f68be6e2f83edb122c3e00a3e4872bc8efa7
tolves <tov.sharing@gmail.com>
2020-11-29 16:06:37 +0000

    delete authority

--- app/controllers/admin/role_authorities_controller.rb
+++ app/controllers/admin/role_authorities_controller.rb
@@ -2,17 +2,32 @@
 
 module Admin
   class RoleAuthoritiesController < ApplicationController
+    before_action :find_role
     def new
-      @role        = Role.find(params[:role_id])
       @authority   = @role.role_authorities.build
+      fresh_when last_modified: @role.updated_at.utc, etag: @role
       @controllers = controllers
+
     end
 
     def create
-      p params
+      @role.role_authorities.create role_authorities_params
+      flash.notice = 'Attach Authorities Successful'
+      redirect_to admin_role_path(@role) and nil
+    rescue StandardError => e
+      flash.alert = e
+      redirect_to admin_role_role_authorities_path(@role)
     end
 
-    def destroy; end
+    def destroy
+      authority = @role.role_authorities.find(params[:id])
+      @role.role_authorities.delete authority
+      flash.notice = 'Remove Authority Successful'
+    rescue StandardError => e
+      flash.alert = e
+    ensure
+      redirect_to admin_role_path(@role)
+    end
 
     def actions
       find_by_controller
@@ -45,6 +60,24 @@ module Admin
         controllers.push ['Admin::' + p, 'Admin::' + p + 'Controller']
       end
       controllers
+    end
+
+    def params_actions
+      params.require(:role_authority).permit(:controller_name)
+      params.require(:role_authority).require(:action_names).permit!
+    end
+
+    def role_authorities_params
+      action_names = []
+      params[:role_authority][:action_names].each { |key, value| action_names.push value }
+      {
+        controller_name: params[:role_authority][:controller_name],
+        action_names:    action_names
+      }
+    end
+
+    def find_role
+      @role = Role.find(params[:role_id])
     end
   end
 end
