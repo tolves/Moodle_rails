522911e8a8363bb0be8761569c041457ec017d9f
tolves <tov.sharing@gmail.com>
2020-11-29 19:38:34 +0000

    add validates

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
   has_many :users
-  has_many :role_authorities
+  has_many :role_authorities, validate: true
   # has_many :type_policies, as: :subject, validate: true
 end
--- app/models/role_authority.rb
+++ app/models/role_authority.rb
@@ -1,3 +1,6 @@
 class RoleAuthority < ApplicationRecord
   belongs_to :role
+  validates_presence_of :role_id, message: "can't be nil"
+  validates_presence_of :action_names, message: "can't be nil"
+  validates :role_id, uniqueness: {scope: %i[controller_name], message: 'should only one'}
 end
